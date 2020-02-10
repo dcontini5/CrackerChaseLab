3d20c731b313fbff6caf4f442d8e42990d569bef
dcontini5 <devcontini@gmail.com>
2020-02-10 14:53:39 +0000

    Removed usings from game1.cs and added .gitignore

--- CrackerChase/Game1.cs
+++ CrackerChase/Game1.cs
@@ -2,10 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Audio;
-
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CrackerChase
 {
